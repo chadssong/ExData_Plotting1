0c9c0ac77df0290b50fa35e8d17afa2f8193e774
chadssong <chadssong@naver.com>
2015-02-09 01:53:24 +0900

    final project1

--- plot4.R
+++ plot4.R
@@ -9,15 +9,26 @@ temp_DateTime<- paste(data_new$Date, data_new$Time, sep="/")
 data_new$DateTime <- strptime(temp_DateTime, "%d/%m/%Y/%H:%M:%S")
 rownames(data_new) <- 1:nrow(data_new)
 
-###### Plot 3. 
+###### Plot 4. 
 
-png(filename = "plot3.png", width = 480, height = 480, units = "px", bg = "transparent")
+png(filename = "plot4.png", width = 480, height = 480, units = "px", bg = "transparent")
+par(mfrow=c(2,2))
 
+# 1
+plot(data_new$DateTime, as.numeric(data_new$Global_active_power), type="l", xlab="", ylab="Global Active Power")
+
+# 2
+plot(data_new$DateTime, as.numeric(data_new$Voltage), type="l", xlab="datetime", ylab="Voltage")
+
+# 3
 plot(data_new$DateTime, data_new$Sub_metering_1,type="n", xlab="", ylab="Energy sub metering")
 lines(data_new$DateTime, data_new$Sub_metering_1, col = "black")
 lines(data_new$DateTime, data_new$Sub_metering_2, col = "red")
 lines(data_new$DateTime, data_new$Sub_metering_3, col = "blue")
+legend("topright", lwd=1, bty="n" ,col=c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
 
-legend("topright", lwd=1 ,col=c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
+# 4
+plot(data_new$DateTime, as.numeric(data_new$Global_reactive_power), type="l", xlab="datetime", ylab="Global_reactive_power")     
+
 
 dev.off()
